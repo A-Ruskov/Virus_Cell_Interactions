76dd54befad3277c9299892871ecfce1bf75480f
A-Ruskov <57944132+A-Ruskov@users.noreply.github.com>
2022-04-07 12:45:25 +0100

    Updated the batch runner

--- batchRunner.sh
+++ batchRunner.sh
@@ -1,4 +1,5 @@
-for i in {1..5}
+for i in {1..10}
 do
-   mpirun -n 16  ./bin/Virus_Cell_Model.exe ./props/config.props ./props/model.props
+   mpirun -n 64  ./bin/Virus_Cell_Model.exe ./props/config.props ./props/model.props
+   echo "Iteration $i completed!"
 done
